15e607573f3de66bb13e464948bc30205a80266c
floan <floan@usc.edu>
2021-02-15 08:15:41 +0500

    server works, haven't compiled yet

---
+++ src/key_value_store/server/kvstore_server.cpp
@@ -0,0 +1,43 @@
+#include "kvstore_server.h"
+
+#include <vector>
+
+Status KeyValueStoreImpl::put(ServerContext* context, 
+    const PutRequest* request, PutReply* response) override {
+  bool result = store_.Put(request->key(), request->value());
+  if (result) {
+    return Status::OK;
+  } else {
+    return Status::ALREADY_EXISTS;
+  }
+}
+
+Status KeyValueStoreImpl::get(ServerContext* context,
+    ServerReaderWriter<GetReply, GetRequest>* stream) override {
+  // TODO: ASK WHY WE RECIEVE A STREAM OF REQUESTS 
+  // BECAUSE THE VALUES WILL BE SENT IN ONE RESPONSE 
+  // SO WE WILL NOT KNOW WHICH VALUE IS ASSOCIATED WITH
+  // WHICH KEY 
+
+  GetRequest request;
+  while (stream->Read(&request)) {
+    // Get the value vector and store it in values
+    std::vector<std::string> values(store_.Get(request.key()));
+    for (std::string s : values) {
+      GetReply response;
+      response.set_value(s);
+      stream->Write(response);
+    }
+  }
+  return Status::OK;
+}
+
+Status KeyValueStoreImpl::remove(ServerContext* context,
+    const RemoveRequest* request, RemoveReply* response) override {
+  bool result = store_.Remove(request->key());
+  if (result) {
+    return Status::OK;
+  } else {
+    return Status::NOT_FOUND;
+  }
+}
