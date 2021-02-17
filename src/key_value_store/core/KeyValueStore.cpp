f0848366ceb882bf70760319ab1ae648797c4ba2
floan <floan@usc.edu>
2021-02-17 10:35:26 +0500

    branch updates, shifting these to other branch

--- src/key_value_store/core/KeyValueStore.cpp
+++ src/key_value_store/core/KeyValueStore.cpp
@@ -11,47 +11,39 @@ typedef std::unordered_map<std::string,
 
 bool KeyValueStore::Put(std::string key, std::string value) {
   bool result; 
-  lock_.lock();
+  const std::lock_guard<std::mutex> lock(lock_);
   // If this key already exists in the store
   if (kv_store_.find(key) != kv_store_.end()) {
     // If user puts the same function
     if (CheckIfExists(kv_store_[key], value)) {
       result = false;
     } else {
-      int size = kv_store_[key].size();
       kv_store_[key].push_back(value);
-      // If the value was not inserted
-      if (size >= kv_store_[key].size()) {
-        result = false;
-      } else {
-        result = true;
-      }
+      result = true;
     }
   } else { // If they key doesn't exist
     //Create a new vector with `value`
     result = kv_store_.insert({key, {value}}).second;
   }
-  lock_.unlock();
+  //lock_guard releases the lock when the mutex goes out of scope
   return result; 
 }
 
 std::vector<std::string> KeyValueStore::Get(std::string key) {
   std::vector<std::string> result;
-  lock_.lock();
+  const std::lock_guard<std::mutex> lock(lock_);
   if (kv_store_.find(key) == kv_store_.end()) {
     result = {}; 
   } else {
     result = kv_store_[key];
   }
-  lock_.unlock();
   return result;
 }
 
 bool KeyValueStore::Remove(std::string key) {
   size_t result; //Returns 1 or 0
-  lock_.lock();
+  const std::lock_guard<std::mutex> lock(lock_);
   result = kv_store_.erase(key);
-  lock_.unlock();
   return result;
 }
 
