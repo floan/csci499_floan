dab4270bff3959912d77c7927af3caf4c76f0b9f
floan <floan@usc.edu>
2021-02-20 12:13:24 +0500

    naive implementation of functions done

--- src/caw/functions/caw_functions.cpp
+++ src/caw/functions/caw_functions.cpp
@@ -63,24 +63,24 @@ Status RegisterUser(Any& EventRequest, Any& EventReply, KeyValueStoreInterface& 
   return status;
 }
 
-Status PostCaw(Any& EventRequest, KeyValueStoreInterface& kvstore) {
+Status PostCaw(Any& EventRequest, Any& EventReply KeyValueStoreInterface& kvstore) {
   CawRequest request;
+  CawReply response;
   EventRequest.UnpackTo(&request);
   std::vector<std::string> userList = kvstore.Get("caw_users");
+  std::vector<std::string> all_caws = kvstore.Get("all_caws");
   Status status; 
   if (!isInList(userList, request.username())) {
     status = Status(StatusCode::NOT_FOUND, "The provided username does not exist.");
+  } else if (request.parent_id() != "" && !isInList(all_caws, request.parent_id())) {
+    status = Status(StatusCode::NOT_FOUND, "The provided parent_id does not exist.");
   } else {
-    // all_caws stores the id in string
-    // This is so we can remove caws in the future
-    // If a cawid was based on the size of the caw array
-    // If we stored 6 caws (ids 1-6) and then removed 1
-    // The latest caw will have id 6, but id 6 pre-exists
-    std::vector<std::string> all_caws = kvstore.Get("all_caws");
+    // Grab the latest id and increment
     std::string currentCawId = std::to_string(
       std::stoi(all_caws[all_caws.size() - 1]) + 1);
     std::string username = request.username(); 
     std::string text = request.text();
+    std::string parent_id = request.parent_id();
     Timestamp timestamp; 
     // Now we will begin storing the caw in the kvstore
     // First we will store the caw id 
@@ -92,13 +92,24 @@ Status PostCaw(Any& EventRequest, KeyValueStoreInterface& kvstore) {
     // caw_id[1] = text
     currentCaw.push_back(text);
     // caw_id[2] = parent_id;
-    currentCaw.push_back("");
+    currentCaw.push_back(parent_id);
     // caw_id[3] = timestamp;
     currentCaw.push_back(timestamp);
     // caw_id[4] = children; 
     currentCaw.push_back("");
 
-    kvstore.Put("caw_"+currentCawId, currentCaw);
+    kvstore.Put("caw_" + currentCawId, currentCaw);
+
+    // If the parent is present we want to 
+    // include current caw in parent's childrens
+    if (parent_id != "") {
+      std::vector<std::string> parent_caw;
+      parent_caw = kvstore.Get("caw_" + parent_id);
+      kvstore.Remove("caw_" + parent_id);
+      // Updating and inserting
+      parent_caw[4] += (currentCawId + " ");
+      kvstore.Put("caw_" + parent_id, parent_caw);
+    }
 
     status = Status::OK;
   }
@@ -150,6 +161,41 @@ Status ReadCaw(Any& EventRequest, Any& EventReply, KeyValueStoreInterface& kvsto
   EventReply.PackFrom(response);
   return status;
 }
+
+Status FollowUser(Any& EventRequest, KeyValueStoreInterface& kvstore) {
+  FollowRequest request; 
+  Status status;
+  EventRequest.UnpackTo(&request);
+  std::vector<std::string> userList = kvstore.Get("caw_users");
+  if (!isInList(userList, request.username()) ||
+    !isInList(userList, request.to_follow())) {
+    status = Status(StatusCode::NOT_FOUND, "You have not provided valid usernames");
+  } else {
+    kvstore.Put("caw_user_" + request.username() + "_following", request.to_follow());
+    kvstore.Put("caw_user_" + request.to_follow() + "_followers", request.username());
+    status = Status::OK;
+  }
+  return status;
+}
+
+Status GetProfile(Any& EventRequest, Any& EventReply, KeyValueStoreInterface& kvstore) {
+  ProfileRequest request; 
+  ProfileReply response; 
+  Status status; 
+  EventRequest.UnpackTo(&request);
+  std::vector<std::string> userList = kvstore.Get("caw_users");
+  if (!isInList(userList, request.username())) {
+    status = Status(StatusCode::NOT_FOUND, "Username does not exsist");
+  } else {
+    std::vector<std::string>> followers = kvstore.Get("caw_user_" + request.username() + "_followers");
+    std::vector<std::string>> following = kvstore.Get("caw_user_" + request.username() + "_following");
+    // TODO: FIGURE OUT HOW TO WRITE OT RESPONSE
+    status = Status::OK;
+  }
+  EventReply.PackFrom(response);
+  return status;
+}
+
 
 
 
