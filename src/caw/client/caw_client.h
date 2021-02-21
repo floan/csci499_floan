07c960599f69ed71d447ee5492778d72ba881886
floan <floan@usc.edu>
2021-02-21 16:02:26 +0500

    have set up client

--- src/caw/client/caw_client.h
+++ src/caw/client/caw_client.h
@@ -1,8 +1,11 @@
 #include "caw.pb.h"
+#include <google/protobuf/any.pb.h>
 #include <grpcpp/grpcpp.h>
 
 #include <string>
 #include <vector>
+
+#include <glog/logging.h>
 
 using google::protobuf::Any;
 using grpc::Channel;
@@ -28,14 +31,34 @@ public:
   // Initialize to set up and connect Faz client
   CawClient();
 
-  bool RegisterUser(std::string username);
-  bool PostCaw(std::string username, std::string text);
-  bool ReplyToCaw(std::string username, std::string text, int parent_id);
-  bool FollowUser(std::string username, std::string to_follow);
-  // Returns a vector of the caw thread
+  // Calls the Faz client to perform the
+  // Following operations
+
+  // Registers the user in the kvstore database
+  // Args: the users username string 
+  // Returns: bool to indicate success / failure
+  bool RegisterUser(const std::string& username);
+  // Creates and saves a caw "post" in the kvstore
+  // Args: posters username and text to post
+  // Returns: bool to indicate success / failure
+  bool PostCaw(const std::string& username, const std::string& text);
+  // Creates and saves a caw "reply" in the kvstore
+  // Args: posters username, text to post and id of caw to reply to
+  // Returns: bool to indicate success / failure
+  bool ReplyToCaw(const std::string& username, const std::string& text, int parent_id);
+  // Follows a user
+  // Args: username of person following and username of person
+  //       to follow
+  // Returns: bool to indicate success / failure
+  bool FollowUser(const std::string& username, const std::string& to_follow);
+  // Gets an entire caw thread from the database
+  // Args: Id of the caw whose sub thread user wants 
+  // Returns: a string of the caw threads
   std::vector<std::string> ReadCaw(int caw_id);
-  // Returns a vector of two vectors, followers and following
-  std::vector<std::vector<std::string>> GetProfile(std::string username);
+  // Gets a users following and follower lists 
+  // Args: username of user whose lists we want
+  // Returns: a vector of two vectors, followers and following
+  std::vector<std::vector<std::string>> GetProfile(const std::string& username);
 
 private:
   // An instance of the Faz Client
