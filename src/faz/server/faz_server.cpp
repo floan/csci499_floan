6ac4f63b476f5a6ef6031b39c424911e8d68bcd9
floan <floan@usc.edu>
2021-02-23 15:01:40 +0500

    added faz_server implementation

---
+++ src/faz/server/faz_server.cpp
@@ -0,0 +1,63 @@
+#include "faz_server.h"
+
+FazServiceImpl::FazServiceImpl() {
+  // Initializing the kvstore client to connect 
+  // with its backend over grpc
+  std::string target_str = "localhost:50001";
+  kvstore_(grpc::CreateChannel(
+      target_str, grpc::InsecureChannelCredentials()));
+  // Initializing caw_functions_ to store the function
+  // data
+  caw_functions_["registeruser"] = RegisterUser;
+  caw_functions_["caw"] = PostCaw;
+  caw_functions_["read"] = ReadCaw;
+  caw_functions_["follow"] = FollowUser;
+  caw_functions_["profile"] = GetProfile;
+}
+
+Status FazServiceImpl::hook(ServerContext* context,
+   const HookRequest* request, HookReply* response) {
+  int eventType = request->event_type();
+  std::string functionName = request->event_function();
+  Status status; 
+  // If the key does not exist
+  if (caw_functions_.find(functionName) == caw_functions_.end()) {
+    status = Status(StatusCode::NOT_FOUND, "The given function does not exist");
+  } else {
+    hooked_functions_[eventType] = functionName;
+    status = Status::OK;
+  }
+  return status;
+}
+
+Status FazServiceImpl::unhook(ServerContext* context,
+    const UnhookRequest* request, UnhookReply* response) {
+  int eventType = request->event_type();
+  Status status; 
+  // If the key does not exist
+  if (hooked_functions_.find(eventType) == hooked_functions_.end()) {
+    status = Status(StatusCode::NOT_FOUND, "The given event type does not exist");
+  } else {
+    hooked_functions_.erase(eventType);
+    status = Status::OK;
+  }
+  return status;
+}
+
+Status FazServiceImpl::event(ServerContext* context,
+    const EventRequest* request, EventReply* response) {
+  int eventType = request->event_type();
+  Any& requestPayload = request->payload();
+  Any& responsePayload = response->payload();
+  Status status; 
+  if (hooked_functions_.find(eventType) == hooked_functions_.end()) {
+    status = Status(StatusCode::NOT_FOUND, "The given event type does not exist");
+  } else {
+    std::string functionName = hooked_functions_[eventType];
+    // Now we will call the function using the stored 
+    // caw functions. Functions will modify data inside 
+    // the Any objects.
+    status = caw_functions_[functionName](requestPayload, responsePayload, kvstore_);
+  }
+  return status; 
+}
--- src/faz/server/faz_server.h
+++ src/faz/server/faz_server.h
@@ -1,6 +1,14 @@
+#include "faz.grpc.pb.h"
+
+#include <unordered_map>
+
+#include <google/protobuf/any.pb.h>
 #include <grpcpp/grpcpp.h>
 
-#include "faz.grpc.pb.h"
+#include "../../key_value_store/KeyValueStoreInterface.h"
+#include "../../key_value_store/client/kvstore_client.h"
+#include "../../caw/functions/caw_functions.h"
+
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -16,25 +24,61 @@ using faz::UnhookReply;
 using faz::EventRequest;
 using faz::EventReply;
 
+// This class implements the server side 
+// functionality of Faz
 class FazServiceImpl final :
 public faz::FazService::Service {
  public:
 
-  //Initialize this to set up the KeyValueClient
+  // Initialize this to set up the KeyValueClient
+  // which we will use to communicate with the server
+  // side logic of the keyvaluestore
   FazServiceImpl();
 
+  // This function 'hooks' a function that can
+  // then be called for use by the Faz client
+  // Args: Context (for additional grpc info),
+  //       HookRequest Protobuf Message containing
+  //       an int to represent the event type
+  //       and a string representing function name
+  //       HookReply Protobuf Message, Empty
+  // Returns: GRPC Status indicating success/error
   Status hook(ServerContext* context,
    const HookRequest* request, HookReply* response) override;
+  // This function 'unhooks' a function so that it
+  // may no longer be called by the Faz client
+  // Args: Context (for additional grpc info),
+  //       UnhookRequest Protobuf Message containing
+  //       an int to represent the event type
+  //       to remove
+  //       UnhookReply Protobuf Message, Empty
+  // Returns: GRPC Status indicating success/error
   Status unhook(ServerContext* context,
     const UnhookRequest* request, UnhookReply* response) override;
+  // This function calls on of the 'hooked' functions
+  // Args: Context (for additional grpc info),
+  //       EventRequest Protobuf Message containing
+  //       an int to represent the event type and a 
+  //       payload containing any input data the 
+  //       function may need
+  //       EventReply Protobuf Message containing
+  //       data produced by the hooked function call
+  // Returns: GRPC Status indicating success/error
   Status event(ServerContext* context,
     const EventRequest* request, EventReply* response) override;
 
  private:
-  // Local variable from core class
-  // Will include KeyValueStoreClient
-  // Will include Map for hookedFunctions
-  // Will include Map for unhookedFuncs
-  // By default unhookedfuncs will be initialized to contain
-  // All the functions related to Caw features
+  // This is a string -> function mapping of 
+  // caw functions. 
+  std::unordered_map<std::string, 
+      std::function<Status(Any&, Any&, 
+        KeyValueStoreInterface)>> caw_functions_;
+  // This is an int to string mapping of hooked functions
+  // We will register an event type with a string that will 
+  // correspond to a function name in caw_functions.
+  // We will not directly store functions in this map
+  std::unordered_map<int, std::string> hooked_functions_;
+  // This is they KeyValueStore that Faz will use 
+  // to store function data
+  KeyValueStoreClient kvstore_;
 }; 
