c0dc431188676294e36957b2b6689a5ad6069a5f
floan <floan@usc.edu>
2021-02-23 15:10:19 +0500

    formatting & minor change

--- src/faz/server/faz_server.cpp
+++ src/faz/server/faz_server.cpp
@@ -1,11 +1,10 @@
 #include "faz_server.h"
 
 FazServiceImpl::FazServiceImpl() {
-  // Initializing the kvstore client to connect 
+  // Initializing the kvstore client to connect
   // with its backend over grpc
   std::string target_str = "localhost:50001";
-  kvstore_(grpc::CreateChannel(
-      target_str, grpc::InsecureChannelCredentials()));
+  kvstore_(grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
   // Initializing caw_functions_ to store the function
   // data
   caw_functions_["registeruser"] = RegisterUser;
@@ -15,11 +14,11 @@ FazServiceImpl::FazServiceImpl() {
   caw_functions_["profile"] = GetProfile;
 }
 
-Status FazServiceImpl::hook(ServerContext* context,
-   const HookRequest* request, HookReply* response) {
+Status FazServiceImpl::hook(ServerContext *context, const HookRequest *request,
+                            HookReply *response) {
   int eventType = request->event_type();
   std::string functionName = request->event_function();
-  Status status; 
+  Status status;
   // If the key does not exist
   if (caw_functions_.find(functionName) == caw_functions_.end()) {
     status = Status(StatusCode::NOT_FOUND, "The given function does not exist");
@@ -30,13 +29,15 @@ Status FazServiceImpl::hook(ServerContext* context,
   return status;
 }
 
-Status FazServiceImpl::unhook(ServerContext* context,
-    const UnhookRequest* request, UnhookReply* response) {
+Status FazServiceImpl::unhook(ServerContext *context,
+                              const UnhookRequest *request,
+                              UnhookReply *response) {
   int eventType = request->event_type();
-  Status status; 
+  Status status;
   // If the key does not exist
   if (hooked_functions_.find(eventType) == hooked_functions_.end()) {
-    status = Status(StatusCode::NOT_FOUND, "The given event type does not exist");
+    status =
+        Status(StatusCode::NOT_FOUND, "The given event type does not exist");
   } else {
     hooked_functions_.erase(eventType);
     status = Status::OK;
@@ -44,20 +45,21 @@ Status FazServiceImpl::unhook(ServerContext* context,
   return status;
 }
 
-Status FazServiceImpl::event(ServerContext* context,
-    const EventRequest* request, EventReply* response) {
+Status FazServiceImpl::event(ServerContext *context,
+                             const EventRequest *request,
+                             EventReply *response) {
   int eventType = request->event_type();
-  Any& requestPayload = request->payload();
-  Any& responsePayload = response->payload();
-  Status status; 
+  Status status;
   if (hooked_functions_.find(eventType) == hooked_functions_.end()) {
-    status = Status(StatusCode::NOT_FOUND, "The given event type does not exist");
+    status =
+        Status(StatusCode::NOT_FOUND, "The given event type does not exist");
   } else {
     std::string functionName = hooked_functions_[eventType];
-    // Now we will call the function using the stored 
-    // caw functions. Functions will modify data inside 
+    // Now we will call the function using the stored
+    // caw functions. Functions will modify data inside
     // the Any objects.
-    status = caw_functions_[functionName](requestPayload, responsePayload, kvstore_);
+    status = caw_functions_[functionName](request->payload(),
+                                          response->payload(), kvstore_);
   }
-  return status; 
+  return status;
 }
